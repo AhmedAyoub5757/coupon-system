7152b43c39289db250605929de0759f54288524b
AhmedAyoub5757 <kkahmed5757@gmail.com>
2026-09-15 12:07:45 +0500

    model

--- app/Models/Coupon.php
+++ app/Models/Coupon.php
@@ -9,7 +9,21 @@ class Coupon extends Model
 {
     use HasFactory;
 
-    // protected $fillable = [
+   protected $fillable = [
+        'code',
+        'type',
+        'value',
+        'min_cart_value',
+        'usage_limit',
+        'used_count',
+        'expires_at',
+        'is_active',
+    ];
 
-    // ]
+    protected $casts = [
+        'expires_at' => 'datetime',
+        'is_active' => 'boolean',
+        'value' => 'decimal:2',
+        'min_cart_value' => 'decimal:2',
+    ];
 }
